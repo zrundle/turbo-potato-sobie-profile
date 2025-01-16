a2ccfddd2dfe3571f57cece4ccddfe2dbc65e126
Cumbie <bcumbie@una.edu>
2025-01-16 16:32:22 -0600

    yes here it is

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const app = express()
+const port = process.env.PORT || 3000;  
+
 
 //endpoint, middleware(s)
 app.get('/', function (req, res) {
   res.send('Hello Express')
 })
 
-app.listen(3000)
+app.listen(
+  port, 
+  ()=> console.log(
+    `server is running on ... ${port}`
+    )
+  );
